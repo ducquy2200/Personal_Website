d7b8ec47f4ff19fef3a6abd5ba27db60d5f13428
ducquy2200 <ducquy2200@gmail.com>
2021-10-12 04:17:15 -0400

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/Navbar';
 
 function App() {
   return (
-    <h2>Portfolio Project ...</h2>
+    <NavBar/>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import logo from "./logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+const NavBar = () => {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+            <div className="container">
+                <a class="navbar-brand" href="/#"><img className="logo" src={logo} alt="logo..." /></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <FontAwesomeIcon icon={faBars} style={{ color:"#fff" }}/>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/#">About Me</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/#">Experiences</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/#">Projects</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/#">Contacts</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
