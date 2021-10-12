4e6070a61fa3aa59cfa1eb088ae25aaa4a9a31fb
ducquy2200 <ducquy2200@gmail.com>
2021-10-12 12:34:31 -0400

    update header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import NavBar from './components/Navbar';
+import Header from './components/Header';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
-    <NavBar/>
+    <>
+      <Particles
+      className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 50,
+              density: {
+                enable: true,
+                value_area: 900
+              },
+            },
+            shape: {
+              type: "star",
+              stroke: {
+                width: 6,
+                color: "#f9ab00"
+              }
+            },
+            size: {
+              value: 3
+            }
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "repulse"
+              }
+            }
+          }
+        }} />
+      <NavBar/>
+      <Header/>
+    </>
   );
 }
 
