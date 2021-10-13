6a930214ea4b0d496267adcbd9d31f3dd7e4fd70
ducquy2200 <ducquy2200@gmail.com>
2021-10-13 14:44:50 -0400

    update projects in project page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import project1 from './media/project1.jpg';
-import project2 from './media/project2.jpg';
-import project3 from './media/project3.jpg';
-import project4 from './media/project4.jpg';
+import project1 from './media/stockPlatform.gif';
+import project2 from './media/looRider.gif';
+import project3 from './media/q-chat.gif';
+import project4 from './media/dj-manager.gif';
+import project5 from './media/2048.gif';
+import project6 from './media/Insta-bot.gif';
+import project7 from './media/flappy-bird.gif';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearchPlus } from '@fortawesome/free-solid-svg-icons';
 import { PopupboxManager, PopupboxContainer } from 'react-popupbox';
@@ -13,7 +16,12 @@ const Project = () => {
         const content = (
             <>
                 <img className="project-image-popupbox" src={project1} alt="project1" />
-                <p>random</p>
+                <p>
+                    <ul>
+                        <li>A web application which features real time stock statistics and makes possible predictions based on past and current prices</li>
+                        <li>Data is scrapped from Yahoo Finance using <b>Pandas and Beautiful Soup</b>, displayed basic info on browser using <b>Matplotlib and Mplfinance</b>, and used <b>Long Short Term Memory</b> (<b>LSTM</b> – implemented using <b>Tensorflow</b>) to predict future stock prices</li>
+                    </ul>
+                </p>
                 <b>GitHub: </b>
                 <a className="hyper-link" onClick={() => window.open("https://github.com/ducquy2200/Stock_Webapp", "_blank")} href='/#'>https://github.com/ducquy2200/Stock_Webapp</a>
             </>
@@ -24,7 +32,7 @@ const Project = () => {
             config: {
                 titleBar: {
                     enable: true,
-                    text: "Project 1"
+                    text: "Stock Price Prediction Web Application"
                 },
                 faseIn: true,
                 faseInSpeed: 500
@@ -36,7 +44,12 @@ const Project = () => {
         const content = (
             <>
                 <img className="project-image-popupbox" src={project2} alt="project2" />
-                <p>random</p>
+                <p>
+                    <ul>
+                        <li>A model of how an autonomous car can be directed by a satellite using computer vision to optimize the path taken by vehicles</li>
+                        <li>A self-driving Arduino car which finds the shortest path by using <b>OpenCV</b> to analyzing the picture of the map and uses <b>A-star algorithm</b> to find the most efficient path in addition to a visualization using <b>Pygame</b></li>
+                    </ul>
+                </p>
                 <b>GitHub: </b>
                 <a className="hyper-link" onClick={() => window.open("https://github.com/ducquy2200/LooRider", "_blank")} href='/#'>https://github.com/ducquy2200/LooRider</a>
             </>
@@ -47,7 +60,7 @@ const Project = () => {
             config: {
                 titleBar: {
                     enable: true,
-                    text: "Project 2"
+                    text: "LooRider - Autonomous Arduino Car"
                 },
                 faseIn: true,
                 faseInSpeed: 500
@@ -59,7 +72,15 @@ const Project = () => {
         const content = (
             <>
                 <img className="project-image-popupbox" src={project3} alt="project3" />
-                <p>random</p>
+                <p>
+                    <ul>
+                        <li>A web application which allows multiple users to send texts and make friends with each other</li>
+                        <li>Front end is developed using <b>HTML/CSS and Python</b> with user friendly interaction and the back end is built using <b>SocketIO, Flask and Javascript</b> with a secure connection to <b>Heroku database</b></li>
+                    </ul>
+                </p>
+                <b>Demo: </b>
+                <a className="hyper-link" onClick={() => window.open("https://qchat-application.herokuapp.com", "_blank")} href='/#'>https://qchat-application.herokuapp.com</a>
+                <br/>
                 <b>GitHub: </b>
                 <a className="hyper-link" onClick={() => window.open("https://github.com/ducquy2200/qchat-app", "_blank")} href='/#'>https://github.com/ducquy2200/qchat-app</a>
             </>
@@ -70,7 +91,7 @@ const Project = () => {
             config: {
                 titleBar: {
                     enable: true,
-                    text: "Project 3"
+                    text: "QChat Web Application with SocketIO"
                 },
                 faseIn: true,
                 faseInSpeed: 500
@@ -82,7 +103,40 @@ const Project = () => {
         const content = (
             <>
                 <img className="project-image-popupbox" src={project4} alt="project4" />
-                <p>random</p>
+                <p>
+                    <ul>
+                        <li>A web application which allows users to manage clients, events, etc (especially designed for DJs)</li>
+                        <li>Front end is developed with Javascript using <b>Vue.js framework</b> (very user friendly) and the back end is built using <b>PHP with MySQL database</b> with RESTful API built from scratch and hosted through <b>FTP</b> on <b>cPanel</b></li>
+                    </ul>
+                </p>
+                <b>Demo: </b>
+                <a className="hyper-link" onClick={() => window.open("https://app.discjockeymanager.com", "_blank")} href='/#'>https://app.discjockeymanager.com</a>
+            </>
+        )
+        PopupboxManager.open({ content })
+        PopupboxManager.update({
+            content,
+            config: {
+                titleBar: {
+                    enable: true,
+                    text: "DJ Jockey Manager - Web Application for client management"
+                },
+                faseIn: true,
+                faseInSpeed: 500
+            }
+        })
+    }
+
+    const openPopupboxProject5 = () => {
+        const content = (
+            <>
+                <img className="project-image-popupbox" src={project5} alt="project5" />
+                <p>
+                    <ul>
+                        <li>A full-fledged functional 2048 game built under principle of OOP</li>
+                        <li>The game was built using <b>Java</b> with <b>Flask framework</b> to give visual appeal and user-friendly vibes</li>
+                    </ul>
+                </p>
                 <b>GitHub: </b>
                 <a className="hyper-link" onClick={() => window.open("https://github.com/ducquy2200/2048", "_blank")} href='/#'>https://github.com/ducquy2200/2048</a>
             </>
@@ -93,7 +147,63 @@ const Project = () => {
             config: {
                 titleBar: {
                     enable: true,
-                    text: "Project 4"
+                    text: "2080 - Java edition"
+                },
+                faseIn: true,
+                faseInSpeed: 500
+            }
+        })
+    }
+
+    const openPopupboxProject6 = () => {
+        const content = (
+            <>
+                <img className="project-image-popupbox" src={project6} alt="project6" />
+                <p>
+                    <ul>
+                        <li>A simple but function Instagram bot which can be used to track who you follow but do not follow you back</li>
+                        <li>The application was built using <b>Python</b> and <b>Selenium framework with Chrome Webdriver</b> to be fully automated</li>
+                    </ul>
+                </p>
+                <b>GitHub: </b>
+                <a className="hyper-link" onClick={() => window.open("https://github.com/ducquy2200/Insta_Bot", "_blank")} href='/#'>https://github.com/ducquy2200/Insta_Bot</a>
+            </>
+        )
+        PopupboxManager.open({ content })
+        PopupboxManager.update({
+            content,
+            config: {
+                titleBar: {
+                    enable: true,
+                    text: "Insta Bot - Best tool to know who does not follow you back"
+                },
+                faseIn: true,
+                faseInSpeed: 500
+            }
+        })
+    }
+
+    const openPopupboxProject7 = () => {
+        const content = (
+            <>
+                <img className="project-image-popupbox" src={project7} alt="project7" />
+                <p>
+                    <ul>
+                        <li>A mock-up of the famous game Flappy Bird with fully functional and 100% match with original sounds and animations</li>
+                        <li>The game was built using <b>Python with Pygame</b> while also is planned to have <b> Tensorflow AI/ML NEAT</b> to make it play by itself</li>
+                    </ul>
+                </p>
+                <b>GitHub: </b>
+                <a className="hyper-link" onClick={() => window.open("https://github.com/ducquy2200/Flappy_Bird", "_blank")} href='/#'>https://github.com/ducquy2200/Flappy_Bird</a>
+            </>
+        )
+        PopupboxManager.open({ content })
+        PopupboxManager.update({
+            content,
+            config: {
+                titleBar: {
+                    enable: true,
+                    text: "Flappy Bird - Pygame exploration with infinite future AI application"
                 },
                 faseIn: true,
                 faseInSpeed: 500
@@ -122,7 +232,22 @@ const Project = () => {
                         <FontAwesomeIcon className="project-icon" icon={faSearchPlus} />
                     </div>
                     <div className="project-image-box" onClick={openPopupboxProject4}>
-                        <img className="project-image" src={project4} alt="project4"/>
+                        <img className="project-image" src={project4} alt="project4" />
+                        <div className="overflow"></div>
+                        <FontAwesomeIcon className="project-icon" icon={faSearchPlus} />
+                    </div>
+                    <div className="project-image-box" onClick={openPopupboxProject5}>
+                        <img className="project-image" src={project5} alt="project5"/>
+                        <div className="overflow"></div>
+                        <FontAwesomeIcon className="project-icon" icon={faSearchPlus} />
+                    </div>
+                    <div className="project-image-box" onClick={openPopupboxProject6}>
+                        <img className="project-image" src={project6} alt="project6" />
+                        <div className="overflow"></div>
+                        <FontAwesomeIcon className="project-icon" icon={faSearchPlus} />
+                    </div>
+                    <div className="project-image-box" onClick={openPopupboxProject7}>
+                        <img className="project-image" src={project7} alt="project7" />
                         <div className="overflow"></div>
                         <FontAwesomeIcon className="project-icon" icon={faSearchPlus} />
                     </div>
