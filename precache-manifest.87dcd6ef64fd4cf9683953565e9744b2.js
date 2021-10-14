acb41c197bb015e9af62065912173d45104b66b2
ducquy2200 <ducquy2200@gmail.com>
2021-10-13 21:19:38 -0400

    Updates

---
+++ precache-manifest.87dcd6ef64fd4cf9683953565e9744b2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd2a3ff06d60b9bb504d963f245008f",
+    "url": "/ddquy.com/index.html"
+  },
+  {
+    "revision": "7727e37ee0e46d1cb4cd",
+    "url": "/ddquy.com/static/css/2.adc0603d.chunk.css"
+  },
+  {
+    "revision": "0a14cfa66265bf48bf0f",
+    "url": "/ddquy.com/static/css/main.6a4de505.chunk.css"
+  },
+  {
+    "revision": "7727e37ee0e46d1cb4cd",
+    "url": "/ddquy.com/static/js/2.793464d2.chunk.js"
+  },
+  {
+    "revision": "0e2a662d93b0eea03d05724231468383",
+    "url": "/ddquy.com/static/js/2.793464d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec5ed1484b047373c72",
+    "url": "/ddquy.com/static/js/3.4a9fb447.chunk.js"
+  },
+  {
+    "revision": "0a14cfa66265bf48bf0f",
+    "url": "/ddquy.com/static/js/main.1ce1ea16.chunk.js"
+  },
+  {
+    "revision": "1f8ec2ad76cfc933b5c9",
+    "url": "/ddquy.com/static/js/runtime-main.82dbb162.js"
+  },
+  {
+    "revision": "4d98ec3155a4e0f0b4d0232b2e0335e7",
+    "url": "/ddquy.com/static/media/2048.4d98ec31.gif"
+  },
+  {
+    "revision": "e77e884577f10ce32ca92efa8ac5ef3a",
+    "url": "/ddquy.com/static/media/Insta-bot.e77e8845.gif"
+  },
+  {
+    "revision": "36d69819d9d18d1804f408757765aaf7",
+    "url": "/ddquy.com/static/media/almen-ng.36d69819.jpg"
+  },
+  {
+    "revision": "3633f02fab6d92f2184435c60d7bdb9f",
+    "url": "/ddquy.com/static/media/dj-manager.3633f02f.gif"
+  },
+  {
+    "revision": "e3c77c1de11faaf8a57a94dfca7614e4",
+    "url": "/ddquy.com/static/media/flappy-bird.e3c77c1d.gif"
+  },
+  {
+    "revision": "995cd269aefe5e4a9b7dc4f452289a4a",
+    "url": "/ddquy.com/static/media/jordan-st-jacques.995cd269.jpg"
+  },
+  {
+    "revision": "b8a17f76b4030a0d5b336e3150c0f760",
+    "url": "/ddquy.com/static/media/joy-xu.b8a17f76.jpg"
+  },
+  {
+    "revision": "cb1856c0c6e4d5b27a32028b1bc35b87",
+    "url": "/ddquy.com/static/media/logo.cb1856c0.png"
+  },
+  {
+    "revision": "9f90167fb00f18014a6d84bc3babec21",
+    "url": "/ddquy.com/static/media/looRider.9f90167f.gif"
+  },
+  {
+    "revision": "afdc56a7e2fd6c229d0cfe9221835dc4",
+    "url": "/ddquy.com/static/media/profile-pic.afdc56a7.png"
+  },
+  {
+    "revision": "1f56fc3f01b15b0837e0b83d34803087",
+    "url": "/ddquy.com/static/media/q-chat.1f56fc3f.gif"
+  },
+  {
+    "revision": "364a88676b11afe29ade0bfcd727d006",
+    "url": "/ddquy.com/static/media/razmin-soltani.364a8867.jpg"
+  },
+  {
+    "revision": "7fa6eb5fefb76a0369614e75bc46a1dd",
+    "url": "/ddquy.com/static/media/reference-bg.7fa6eb5f.jpg"
+  },
+  {
+    "revision": "6923a163c09764e8476efab75822c438",
+    "url": "/ddquy.com/static/media/stockPlatform.6923a163.gif"
+  }
+]);
