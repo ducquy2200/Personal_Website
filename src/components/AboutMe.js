603a547d94f967b692568c02f1c0d87a9379cc9b
ducquy2200 <ducquy2200@gmail.com>
2022-04-22 23:34:13 -0400

    fix p tag being ul tag parent error

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -15,33 +15,29 @@ const AboutMe = () => {
                     <p>
                         I am a dedicated and hardworking 2nd Year Software Engineering student at University of Waterloo who has an inspiring desire to learn and improve in developing modern technology. I possess a continuous improving mindset and creativity which allows me to acquire success and productive results at both school and work. Here is a summary of my skills:
                     </p>
-                    <p>
-                        <ul>
-                            <li>Resourceful work and volunteering experiences with great social and technological involvements</li>
-                            <li>Excellent problem solving and interpersonal skills</li>
-                            <li>A dedicated individual with a inspired desire to learn and improve in developing modern technology</li>
-                            <li>Exemplary teamwork coordination skills capable of working in both small and large-scale organizations</li>
-                            <li>Diligent and responsible with the ability to work in fast-paced workplaces while ensuring sufficient progress</li>
-                        </ul>
-                    </p>
+                    <ul>
+                        <li>Resourceful work and volunteering experiences with great social and technological involvements</li>
+                        <li>Excellent problem solving and interpersonal skills</li>
+                        <li>A dedicated individual with a inspired desire to learn and improve in developing modern technology</li>
+                        <li>Exemplary teamwork coordination skills capable of working in both small and large-scale organizations</li>
+                        <li>Diligent and responsible with the ability to work in fast-paced workplaces while ensuring sufficient progress</li>
+                    </ul>
                     <p>
                         Here are some of my technical skills:
                     </p>
-                    <p>
-                        <ul>
-                            <li><b>Programming Languages:</b> JavaScript, PHP, Java, Dart, Python, C/C++, C#, Ruby, Git, HTML/CSS, SQL, Elixir, Bash</li>
-                            <li><b>Frontend:</b> React.js, Vue.js, AR.js, Flutter, Boostrap, JQuery</li>
-                            <li><b>Backend/Database:</b> Node.js, Laravel, Selenium, Hibernate, Heroku, Django, Flutter, Ruby on Rails, MySQL, PostgreSQL</li>
-                            <li><b>Unit/Feature Testing:</b> PHPUnit, JUnit, Cypress</li>
-                            <li><b>Hosting Platform:</b> AWS, cPanel, Heroku, GitHub (Page), Unix Web Server</li>
-                            <li><b>Web Server:</b> Nginx, Gunicorn</li>
-                            <li><b>Artificial Intelligence/Machine Learning/Data Science libraries:</b> Tensorflow, Scikit-learn, Numpy, Pandas, Matplotlib, BeautifulSoup</li>
-                            <li><b>Hardware: </b> Arduino, Raspberry Pi (and related add-ons)</li>
-                            <li><b>Tools/Software:</b> Slack, Trello, Jira, cPanel, Postman, AWS, Azure, Git</li>
-                            <li><b>Text Editors:</b> VS Code, IntelliJ, Eclipse, Vim, Atom, Android Studio</li>
-                            <li><b>Document Platform:</b> Power BI, Microsoft Office, Microsoft Office 365, Google Drive</li>
-                        </ul>
-                    </p>
+                    <ul>
+                        <li><b>Programming Languages:</b> JavaScript, PHP, Java, Dart, Python, C/C++, C#, Ruby, Git, HTML/CSS, SQL, Elixir, Bash</li>
+                        <li><b>Frontend:</b> React.js, Vue.js, AR.js, Flutter, Boostrap, JQuery</li>
+                        <li><b>Backend/Database:</b> Node.js, Laravel, Selenium, Hibernate, Heroku, Django, Flutter, Ruby on Rails, MySQL, PostgreSQL</li>
+                        <li><b>Unit/Feature Testing:</b> PHPUnit, JUnit, Cypress</li>
+                        <li><b>Hosting Platform:</b> AWS, cPanel, Heroku, GitHub (Page), Unix Web Server</li>
+                        <li><b>Web Server:</b> Nginx, Gunicorn</li>
+                        <li><b>Artificial Intelligence/Machine Learning/Data Science libraries:</b> Tensorflow, Scikit-learn, Numpy, Pandas, Matplotlib, BeautifulSoup</li>
+                        <li><b>Hardware: </b> Arduino, Raspberry Pi (and related add-ons)</li>
+                        <li><b>Tools/Software:</b> Slack, Trello, Jira, cPanel, Postman, AWS, Azure, Git</li>
+                        <li><b>Text Editors:</b> VS Code, IntelliJ, Eclipse, Vim, Atom, Android Studio</li>
+                        <li><b>Document Platform:</b> Power BI, Microsoft Office, Microsoft Office 365, Google Drive</li>
+                    </ul>
                 </div>
             </div>
         </div>
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -13,51 +13,43 @@ const Experience = () => {
                         <h3>May 2021 - Aug 2021</h3>
                         <h4>Software Engineer | Back End Developer at Walletifai</h4>
                         <h5><b>Toronto, ON, Canada</b></h5>
-                        <p>
-                            <ul>
-                                <li>Implemented core content management features to centralize user banking info using Java and MySQL and improved approachability by 60%</li>
-                                <li>Developed new push notification and emailing management system to inform users of their financial situation using Java Spring Boot and Cronjob</li>
-                                <li>Optimized database queries and redesigned data fetching algorithms to prevent query overload and improved data search runtime by 80%, resolved a major ongoing issue for the product</li>
-                                <li>Designed a validation system to authenticate webhooks and incoming requests using Java , improving security by 100%</li>
-                                <li><b>Software and framework:</b> Java, Spring Boot, Spring Data, Hibernate, MySQL</li>
-                                <li><b>Other skills:</b> Git (Github), AWS, Data Logs, Concurrency, Unit Test</li>
-                            </ul>
-                        </p>
+                        <ul>
+                            <li>Implemented core content management features to centralize user banking info using Java and MySQL and improved approachability by 60%</li>
+                            <li>Developed new push notification and emailing management system to inform users of their financial situation using Java Spring Boot and Cronjob</li>
+                            <li>Optimized database queries and redesigned data fetching algorithms to prevent query overload and improved data search runtime by 80%, resolved a major ongoing issue for the product</li>
+                            <li>Designed a validation system to authenticate webhooks and incoming requests using Java , improving security by 100%</li>
+                            <li><b>Software and framework:</b> Java, Spring Boot, Spring Data, Hibernate, MySQL</li>
+                            <li><b>Other skills:</b> Git (Github), AWS, Data Logs, Concurrency, Unit Test</li>
+                        </ul>
                         <h4>Software Engineer | Project Manager/Full Stack Developer at Digitera Interactive</h4>
                         <h5><b>Ottawa, ON, Canada</b></h5>
-                        <p>
-                            <ul>
-                                <li>Developed core features for admin dashboard and ecommerce web applications using <b>cPanel, React.js, Vue.js, ES6, PHP, and MySQL</b></li>
-                                <li>Developed various web scraping applications to extract useful information for market analysis using <b>Selenium, Node.js, C#, PHP, Python</b></li>
-                                <li>Applied algorithms and data structure knowledge in solving problems and implementing new features in company's based templates with <b>RESTful API</b> developed from scratch using <b>PHP PDO and MySQL</b> resulted in 80% increase in security and speed</li>
-                                <li>Managed backend and frontend developer teams with proper responsibility assigning through frequent group discussion meetings and project management tools <b>Trello and Jira</b></li>
-                                <li>Mentored co-op students by providing learning materials and code review meetings</li>
-                                <li>Developed backend <b>RESTful API</b> in <b>PHP environment</b></li>
-                                <li>Implemented feature testing with <b>Cypress</b> and backend unit testing using <b>PHPunit</b></li>
-                            </ul>
-                        </p>
+                        <ul>
+                            <li>Developed core features for admin dashboard and ecommerce web applications using <b>cPanel, React.js, Vue.js, ES6, PHP, and MySQL</b></li>
+                            <li>Developed various web scraping applications to extract useful information for market analysis using <b>Selenium, Node.js, C#, PHP, Python</b></li>
+                            <li>Applied algorithms and data structure knowledge in solving problems and implementing new features in company's based templates with <b>RESTful API</b> developed from scratch using <b>PHP PDO and MySQL</b> resulted in 80% increase in security and speed</li>
+                            <li>Managed backend and frontend developer teams with proper responsibility assigning through frequent group discussion meetings and project management tools <b>Trello and Jira</b></li>
+                            <li>Mentored co-op students by providing learning materials and code review meetings</li>
+                            <li>Developed backend <b>RESTful API</b> in <b>PHP environment</b></li>
+                            <li>Implemented feature testing with <b>Cypress</b> and backend unit testing using <b>PHPunit</b></li>
+                        </ul>
                         <h4>Junior Machine Learning Developer at AltaML</h4>
                         <h5><b>Waterloo, ON, Canada</b></h5>
-                        <p>
-                            <ul>
-                                <li>Worked with AltaML and Health Canada on a Machine Learning to analyze public sentiment</li>
-                                <li>Used <b>Natural Language Processing, Sentiment Analysis, and Entity Recognition</b> to evaluate the risk of a Site's operating procedures (for people, processes, and products) which do not meet regulatory requirements and increase the risk to the health of Canadians</li>
-                                <li>Utilized <b>Microsoft Azure</b> to develop a <b>Machine Learning Pipeline</b> with a success rate of 68% of true negative</li>
-                            </ul>
-                        </p>
+                        <ul>
+                            <li>Worked with AltaML and Health Canada on a Machine Learning to analyze public sentiment</li>
+                            <li>Used <b>Natural Language Processing, Sentiment Analysis, and Entity Recognition</b> to evaluate the risk of a Site's operating procedures (for people, processes, and products) which do not meet regulatory requirements and increase the risk to the health of Canadians</li>
+                            <li>Utilized <b>Microsoft Azure</b> to develop a <b>Machine Learning Pipeline</b> with a success rate of 68% of true negative</li>
+                        </ul>
                     </div>
                 </div>
                 <div className="timeline-block timeline-block-left">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h3>May 2021 - Present</h3>
+                        <h3>May 2021 - Dec 2021</h3>
                         <h4>Software Engineer | Firmware Developer at Midnight Sun Solar Race Car Team</h4>
                         <h5><b>Waterloo, ON, Canada</b></h5>
-                        <p>
-                            <ul>
-                                <li>*To be updated</li>
-                            </ul>
-                        </p>
+                        <ul>
+                            <li>*To be updated</li>
+                        </ul>
                     </div>
                 </div>
                 <div className="timeline-block timeline-block-right">
@@ -66,13 +58,11 @@ const Experience = () => {
                         <h3>Jun 2021 - Present</h3>
                         <h4>Software Engineer | Mobile Developer at Felicity</h4>
                         <h5><b>Waterloo, ON, Canada</b></h5>
-                        <p>
-                            <ul>
-                                <li>Developed core features for <b>Flutter</b> productivity mobile application using <b>Dart and Firebase</b> in collaboration with other developers</li>
-                                <li>Improved data loading efficiency and runtime by 300% resulted from decreasing widgets rebuilding frequency and changing widget state</li>
-                                <li>Implemented <b>16 complex animated components</b> using <b>Flutter</b> for universal usage within the app in the collaboration with research and design teams</li>
-                            </ul>
-                        </p>
+                        <ul>
+                            <li>Developed core features for <b>Flutter</b> productivity mobile application using <b>Dart and Firebase</b> in collaboration with other developers</li>
+                            <li>Improved data loading efficiency and runtime by 300% resulted from decreasing widgets rebuilding frequency and changing widget state</li>
+                            <li>Implemented <b>16 complex animated components</b> using <b>Flutter</b> for universal usage within the app in the collaboration with research and design teams</li>
+                        </ul>
                     </div>
                 </div>
                 <div className="timeline-block timeline-block-left">
@@ -81,11 +71,9 @@ const Experience = () => {
                         <h3>Jan 2022 - Apr 2022</h3>
                         <h4>Software Engineer | Data Analyst/Software Developer at City of Kitchener</h4>
                         <h5><b>Waterloo, ON, Canada</b></h5>
-                        <p>
-                            <ul>
-                                <li>*To be updated</li>
-                            </ul>
-                        </p>
+                        <ul>
+                            <li>*To be updated</li>
+                        </ul>
                     </div>
                 </div>
             </div>
